beb67e058e6194fd51aaba515bf1acd77c1efae3
plight <plight@gmail.com>
2015-02-24 21:16:10 -0500

    initial project creation - default MVC app

---
+++ TimeCard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeCard.Startup))]
+namespace TimeCard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
