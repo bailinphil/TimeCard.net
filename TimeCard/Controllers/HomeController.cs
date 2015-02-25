6961dd9a839f61dc95aaab491d920889abd07e61
plight <plight@gmail.com>
2015-02-25 12:30:23 -0500

    connecting to sqlite database, running query, displaying result in home page main header.

--- TimeCard/Controllers/HomeController.cs
+++ TimeCard/Controllers/HomeController.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Data.SQLite;
+
 namespace TimeCard.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            SQLiteConnection conn = new SQLiteConnection(@"Data Source=D:\code\TimeCard\TimeCard\TimeCard\App_Data\time.db;Version=3;");
+            conn.Open();
+            SQLiteCommand query = new SQLiteCommand("select count(*) as days from workday where userId = 2", conn);
+            SQLiteDataReader reader = query.ExecuteReader();
+            int days = 0;
+            while (reader.Read())
+            {
+                days = Convert.ToInt32(reader["days"]);
+            }
+            Console.WriteLine(days);
+            reader.Close();
+            conn.Close();
+
+            ViewBag.Days = days;
             return View();
         }
 
