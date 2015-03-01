7728be64af2012d1810882bc45cd5cdc65ecae16
plight <plight@gmail.com>
2015-03-01 10:18:59 -0500

    bringing together my controller with out-of-box layout

--- TimeCard/Controllers/HomeController.cs
+++ TimeCard/Controllers/HomeController.cs
@@ -12,34 +12,12 @@ namespace TimeCard.Controllers
     {
         public ActionResult Index()
         {
-            SQLiteConnection conn = new SQLiteConnection(@"Data Source=D:\code\TimeCard\TimeCard\TimeCard\App_Data\time.db;Version=3;");
-            conn.Open();
-            SQLiteCommand query = new SQLiteCommand("select count(*) as days from workday where userId = 2", conn);
-            SQLiteDataReader reader = query.ExecuteReader();
-            int days = 0;
-            while (reader.Read())
-            {
-                days = Convert.ToInt32(reader["days"]);
-            }
-            Console.WriteLine(days);
-            reader.Close();
-            conn.Close();
-
-            ViewBag.Days = days;
+            ViewBag.Days = "Welcome";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
--- TimeCard/Models/AccountViewModels.cs
+++ TimeCard/Models/AccountViewModels.cs
@@ -70,7 +70,7 @@ namespace TimeCard.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
